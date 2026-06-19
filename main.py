160441743932248de0c789abadb0902362c3d6b8
merakiakire88 <merakiakire@gmail.com>
2026-06-19 13:13:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# --- CONFIG UTAMA WEB ---
+st.set_page_config(page_title="Toko Online Saya", page_icon="🛍️", layout="centered")
+
+# --- 1. DEFINISIKAN HALAMAN-HALAMAN ---
+# Halaman yang bisa dilihat oleh SEMUA ORANG (Pelanggan)
+halaman_pelanggan = [
+    st.Page("views/dashboard.py", title="Katalog Produk", icon="🏠", default=True),
+    st.Page("views/keranjang.py", title="Keranjang Belanja", icon="🛒"),
+]
+
+# Halaman yang HANYA BISA dilihat oleh Admin
+halaman_admin = [
+    st.Page("admin_views/admin_panel.py", title="Panel Kontrol Admin", icon="🔒")
+]
+
+# --- 2. LOGIKAL PENYEMBUNYIAN HALAMAN ---
+# Mengambil parameter dari URL browser (misal: websaya.streamlit.app/?role=admin)
+query_params = st.query_params
+
+if query_params.get("role") == "admin":
+    # Jika di URL terdeteksi '?role=admin', gabungkan halaman pelanggan + admin
+    navigasi = st.navigation(halaman_pelanggan + halaman_admin)
+else:
+    # Jika pelanggan biasa (tanpa URL rahasia), halaman admin TIDAK AKAN PERNAH dimuat
+    navigasi = st.navigation(halaman_pelanggan)
+
+
+# --- 3. JALANKAN NAVIGASI ---
+navigasi.run()
